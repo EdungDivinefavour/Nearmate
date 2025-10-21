7b8f64a10e93558c28fba3a718c5bfdfd045f985
EdungDivinefavour <divine.edung@bestpass.com>
2025-10-20 23:27:15 -0300

    Cleaned up forgotpassword

--- app/src/main/java/ca/unb/mobiledev/nearmate/Nearmate.kt
+++ app/src/main/java/ca/unb/mobiledev/nearmate/Nearmate.kt
@@ -1,11 +1,9 @@
 package ca.unb.mobiledev.nearmate
 
 import android.app.Application
-import ca.unb.mobiledev.nearmate.services.LocationService
 
 class Nearmate: Application() {
     override fun onCreate() {
         super.onCreate()
-
     }
 }
